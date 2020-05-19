9c082256f535b2125444df4ef6262249f987d6d0
sricharan <sricharan@biztekinnovations.com>
2020-05-19 15:13:08 -0400

    initial setup

--- server.js
+++ server.js
@@ -1,25 +1,7 @@
-const fs = require('fs');
-var dotenvFiles = [
-  `${paths.dotenv}.${NODE_ENV}.local`,
-  `${paths.dotenv}.${NODE_ENV}`,
-  // Don't include `.env.local` for `test` environment
-  // since normally you expect tests to produce the same
-  // results for everyone
-  NODE_ENV !== 'test' && `${paths.dotenv}.local`,
-  paths.dotenv,
-].filter(Boolean);
 
-dotenvFiles.forEach(dotenvFile => {
-  if (fs.existsSync(dotenvFile)) {
-    require('dotenv').config({
-      path: dotenvFile,
-    });
-  }
-});
-
-// if(process.env.NOD_ENV !== 'production') {
-//   require('dotenv').config();
-// }
+if(process.env.NOD_ENV !== 'development') {
+  require('dotenv').config();
+}
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
